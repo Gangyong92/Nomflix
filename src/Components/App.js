e2723830f533d83b461283701d98e726608a7c38
Gangyong92 <hyeonggang@naver.com>
2021-01-12 17:53:12 +0900

    #1.1 Nomflix ~#2.2

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,12 @@
-import React, { Conponent } from "react";
+import React, { Component } from "react";
 import Router from "Components/Router";
+import Header from "Components/Header";
 
-class App extends Conponent {
+class App extends Component {
   render() {
     return (
       <>
+        <Header />
         <Router />
       </>
     );
